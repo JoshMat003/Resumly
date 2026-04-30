0d5fadb40d880915ac4184ac436283cbbb027631
JoshMat003 <JGMathew003@gmail.com>
2026-04-30 04:25:12 +0000

    changed LLM

--- ResumeProject - Copy/resume_tailor_app.py
+++ ResumeProject - Copy/resume_tailor_app.py
@@ -399,7 +399,7 @@ defaults = {
     "first_name": "",
     "last_name": "",
     "full_name": "",
-    "model": "llama-3.3-70b-versatile", # Change default model
+    "model": "llama-3.1-8b-instant", # Change default model
     "groq_api_key": "",                 # New key storage
 }
 for k, v in defaults.items():
